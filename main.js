edf9ba6b17dbab64c0aaf7e6c1669ab5b52570fe
lai2-01 <lai2-01@itismeucci.com>
2022-02-23 13:48:37 +0100

    completazione elimina e aggiunta

--- main.js
+++ main.js
@@ -43,28 +43,57 @@ var data =
     
     }
 ];
-
-var nextId = 1006;
-
+var nextId = 10006;
+var btnElimina = "<button class='btn btn-danger elimina'>Elimina</button>";
+var btnModifica = "<button class='btn btn-danger Modifica'>Modifica</button>";
 
 $(document).ready(function()
 {
-    displayTable();
+    displayTable(),
+    configuraAggiungi(),
+    elimina()
 });
 
 var u;
 
 function displayTable() 
 {
+    var u ='';
     $.each(data, function(id, value)
     {
         u += '<tr>';
         u += '<td>' + value.id + '</td>';
         u += '<td>' + value.firstName + '</td>';
         u += '<td>' + value.lastName + '</td>';
-        u += '<td>' +'elimina'+ '</td>';
+        u += '<td>' + btnElimina + '</td>';
         u += '</tr>';
     });
-    $("#contenutoTabella").append(u);
+    $("#contenutoTabella").html(u);
 
+}
+
+function configuraAggiungi() {
+    $("#aggiungi").click(function () 
+    {
+        var nome = prompt('Nome');
+        var congnome = prompt('Cognome');
+        console.log(nome, congnome);
+        var n = {
+            "id": nextId,
+            "firstName": nome,
+            "lastName": congnome,
+            "gender": "M",
+        };
+        data.push(n);
+        displayTable();
+        nextId++;
+    });
+}
+
+function elimina() 
+{
+    $(".elimina").click(function () 
+    {
+        $(this).parents("tr").fadeOut("fast");
+    });
 }
